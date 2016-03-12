1cac225ba44a55ed4203cc93daac907c333491b6
Raman Nakarmi <rnakarmi@ucsc.edu>
2016-03-12 15:36:25 -0800

    information extraction for creating workout

---
+++ LogIt/app/src/main/java/danman/logit/editWorkout.java
@@ -0,0 +1,28 @@
+package danman.logit;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+/**
+ * Created by Raman on 3/12/2016.
+ */
+public class editWorkout extends AppCompatActivity {
+
+    SharedPreferences settings;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editworkout);
+
+        settings = PreferenceManager.getDefaultSharedPreferences(this);
+      boolean bob =  settings.getBoolean("workout1",false);
+        Log.i("CHECKINGINFO", Boolean.toString(bob));
+
+    }
+
+}
